311251354542102c28d0f67ec1217d8f550cd044
SyedRahadHossain <syedhossainibnali@gmail.com>
2026-05-21 12:15:51 +0600

    updated index.ts for category

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import express, { Router } from "express";
 import { userRouter } from "../modules/user/user.route";
 import { tutorRouter } from "../modules/tutor/tutor.router";
-
+import { categoryRouter } from "../modules/category/category.router";
 
 // const router = Router();
 const router: Router = express.Router();
 
-
 router.use("/users", userRouter);
 router.use("/tutors", tutorRouter);
+router.use("/categories", categoryRouter);
 
 export default router;
