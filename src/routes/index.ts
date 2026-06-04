2698dc19e55c1df2bb4d8a5b174cee67549c99d3
SyedRahadHossain <syedhossainibnali@gmail.com>
2026-06-04 09:27:46 +0600

    added booking routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express, { Router } from "express";
 import { userRouter } from "../modules/user/user.route";
 import { tutorRouter } from "../modules/tutor/tutor.router";
 import { categoryRouter } from "../modules/category/category.router";
+import { bookingRouter } from "../modules/booking/booking.router";
 
 // const router = Router();
 const router: Router = express.Router();
@@ -9,5 +10,7 @@ const router: Router = express.Router();
 router.use("/users", userRouter);
 router.use("/tutors", tutorRouter);
 router.use("/categories", categoryRouter);
+router.use("/bookings", bookingRouter);
+
 
 export default router;
